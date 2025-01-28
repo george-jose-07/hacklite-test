435b14311084b62c45203c8527e3bd60e869c685
GEORGE JOSE <108619183+george-jose-07@users.noreply.github.com>
2025-01-28 14:49:01 +0530

    Add files via upload

---
+++ html project/index.js
@@ -0,0 +1,61 @@
+// console.log('Hello, world!');
+// console.log(document);
+// let username  = window.prompt('Enter your name');
+// console.log(username);
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// if(true){
+//     var a = 40;
+//     let b = 50;
+// }
+
+// console.log(a);
+// console.log(b);
+// function add(a, b) {
+//     return a + b;
+//   }
+//   console.log(add(5, 3));
+
+// let name = "Alice"; // String
+// let age = 25; // Number
+// let isStudent = true; // Boolean
+// let score = null; // Null
+// let city; // Undefined
+// console.log(typeof name); // Output: string
+// console.log(typeof age); // Output: number
+// console.log(typeof isStudent); // Output: boolean
+// console.log(typeof score); // Output: object (quirk of JS)
+// console.log(typeof city); // Output: undefined
+
+// let age = 20;
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+// let grade = "B";
+// switch (grade) {
+//   case "A":
+//     console.log("Excellent");
+//     break;
+//   case "B":
+//     console.log("Good");
+//     break;
+//   default:
+//     console.log("Needs Improvement");
+// }
+
+// let result = age >= 18 ? "Adult" : "Minor";
+// console.log(result);
+
+// let text = "The rain in Spain falls mainly";
+// let pattern = /rain/;
+// console.log(pattern.test(text)); // true
+// console.log(text.match(pattern)); // ['rain']
+
+const email = document.getElementById('email').value;
+console.log(email);
